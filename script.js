45c0cbf2b40123a90e004f2ff43eb002e7215769
Sinthia <sinthiaanam@gmail.com>
2018-05-30 02:57:25 +0000

    //classwork4 task2

--- script.js
+++ script.js
@@ -3,7 +3,13 @@ var heading = document.getElementById('heading')
 console.log(heading.innerText)
 
 /* Task 2 using plain JavaScript */
-
-
+function addText2(){
+    var task2div = document.getElementById('task2a')
+    var paragraph = document.createElement('p')
+    paragraph.innerText = "Hello World"
+    task2div.appendChild(paragraph)
+}
+    var textBtn = document.getElementById('textBtn2a')
+    textBtn.addEventListener('click', addText2)
 
 /* Task 4 using jQuery */
